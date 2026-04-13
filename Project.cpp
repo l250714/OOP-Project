07da3f20a4ebf84154d3324b3d77c3a5174497d6
l250714 <l250714@lhr.nu.edu.pk>
2026-04-13 14:08:47 +0500

    Project.cpp
    designing academic entity

--- Project.cpp
+++ Project.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+using namespace std;
 
+class AcademicEntity{
+    string name,email,ID;
+public:
+    virtual void displayProfile()=0;
+};
+
+class Student: public AcademicEntity{      
+    int num_courses;
+    char*courses;
+    int semester;
+public:
+    void calculateGPA(){}
+    void viewTranscript(){}
+
+};
+class Regular_Student: public Student{
+    float gpa;
+    float cgpa;
+public:
+    void displayProfile();
+};
+
+class Scholarship_Student: public Regular_Student{
+    string status_flag;
+public:
+    void displayProfile();
+};
+
+class Exchange_Student: public Student{
+public:
+    void displayProfile();
+};
+
+
+class Teacher: public AcademicEntity{
+    char* assigned_courses;
+    int num_courses;
+};
+
+int main(){
+
+    return 0;
+}
