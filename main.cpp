26be3ccf2e7c1c766ec772e28a652803ecfd0e35
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 21:26:04 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 #include "allheaders.h"
 using namespace std;
 
+/* cd "C:\Users\hp\OneDrive\Desktop\Uni study\Sem 2\OOP\Project" g++ -g Academic_Entities.cpp Assessments.cpp Courses.cpp DatabaseManager.cpp Management.cpp Venues.cpp main.cpp -o Project.exe
+./Project.exe */
+
 int main(){
     readStudentdata();
+    cout<<"\nStudent data read";
+    readCoursesdata();
+    cout<<"\nCourses data read";
+    readTeachersdata();
+    cout<<"\nTeachers data read";
+    readVenuesdata();
+    cout<<"\nVenues data read";
+    readSectionsdata();
+    cout<<"\nSections data read";
+    readWeightagesdata();
+    cout<<"\nWeightages data read";
+    readAssessmentsdata();
+    cout<<"\nAssessments data read";
     return 0;
 }
