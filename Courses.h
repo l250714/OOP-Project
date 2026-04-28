5198dc38a88b5eb91b5f3c65f58bdefcc414d4ed
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 21:18:52 +0500

    Update Courses.h

--- Courses.h
+++ Courses.h
@@ -12,30 +12,33 @@ class Student;
 //think about schedule and timings and venue
 class Course{   //base class
 protected:      //makes data members accessible in child classes
-    char* ID;
-    char* name;
+    string ID;
+    string name;
     int TQuizzes, TExams, TAssignments;     //total of each assessment type (in class)
     float exam_duration;
     int credithr;      
     float batch_average;    //out of 100 (in class)
+    string teacherID;
+    vector <Student*> students;
 public:
     Course();
     void set_ID(const char* id);
     void setname(const char* name);
     void setExamduration(float time);
+    void setStudent(Student& obj);
     string getID();
     string getName();
     float getExamDuration();
+    Student& getStudent(int i);
     
 };
 
 //----Types of Courses-----
 
 class Core: public Course{
-    Quiz* quizzes;
-    Exam* exams;
-    Assignment* assignments;
-    vector <Student*> students;
+    vector<Quiz> quizzes;
+    vector<Exam> exams;
+    vector <Assignment> assignments;
     float points;
     char*grade;
 
@@ -43,34 +46,49 @@ public:
     Core();
     void setpoints(float points);
     void setgrade(char* grade);
+    void setQuiz(Quiz& obj);
+    void setExam(Exam& obj);
+    void setAssignment(Assignment& obj);
     float getpoints();
     string getgrade();
+    Exam& getExam(int i);
+    Quiz& getQuiz(int i);
+    Assignment& getAssignment(int i);
 
 };
 
 class Elective: public Course{
-    Assignment* assignments;
+    vector <Assignment> assignments;
     Project final_project;
-    Quiz* quizzes;
+    vector<Quiz> quizzes;
     float points;
     char*grade;
 public:
     Elective();
     void setpoints(float points);
+    void setQuiz(Quiz& obj);
     void setgrade(char* grade);
+    void setLabtasks(Assignment& obj);
+    //void setProject();
     float getpoints();
     string getgrade();
+     Quiz& getQuiz(int i);
+    Assignment& getLabTasks(int i);
 };
 
 class Lab: public Course{
-    Quiz* quizzes;
-    Assignment* Lab_tasks;
+    vector<Quiz> quizzes;
+    vector <Assignment> Lab_tasks;
     float points;
     char*grade;
 public:
     Lab();
     void setpoints(float points);
     void setgrade(char* grade);
+    void setQuiz(Quiz& obj);
+    void setAssignment(Assignment& obj);
+    Quiz& getQuiz(int i);
+    Assignment& getAssignment(int i);
     float getpoints();
     string getgrade();
 };
