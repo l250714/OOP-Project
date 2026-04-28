b526981b664f0a5bb39eeab89dd597da44596a07
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 14:17:21 +0500

    Update Academic_Entities.h

--- Academic_Entities.h
+++ Academic_Entities.h
@@ -4,67 +4,74 @@
 #include <iostream>
 using namespace std;
 #include "Courses.h"
-#include "Assessments.h"
 
 
 class AcademicEntity{       
     string name,email,ID;
-    int num_courses;
-    Course *courses;
+    int num_core, num_lab, num_elective;
+    Core *core; Elective *elective; Lab *labs;
 public:
     virtual void displayProfile()=0;
 
     string getName();
     string getEmail();
     string getID();
-    int getNumCourses();
+    int getNumCore();
+    int getNumElective();
+    int getNumLab();
+    Core* getCore(int i);
+    Elective* getElective(int i);
+    Lab* getLab(int i);
 
     void setName(string name);
     void setEmail(string email);
     void setID(string ID);
-    void setNumCourses(int num);
+    void setNumCore(int num);
+    void setNumElective(int num);
+    void setNumLab(int num);
 };
 
 class Student: public AcademicEntity{      
     int semester;
 public:
-    void calculateGPA();
     void viewTranscript();
-    char* getcourses();
     int getSemester();
     void displayProfile();
-
-
     /* void addCourse(char* course);*/
     //void updateCourse(char );    
     void setSemester(int sem);
-
-
+    virtual string getType()=0;
 };
 class Regular_Student: public Student{
     float gpa;  //CGPA
     float sgpa;
 
 public:
+    Regular_Student();
+    Regular_Student(string id,string name, string email, int semester,float gpa);
     void displayProfile();
     float GPAcalculation();
-
+    string getType();
     float getGPA();
     float getSGPA();
+    void setgpa(float gpa);
 };
 
 class Scholarship_Student: public Regular_Student{
     string status_flag;
 public:
+    Scholarship_Student(string id, string name, string email, int semester, float gpa);
     void displayProfile();
-    float GPA();
-
+    //float GPA();
+    string getType();
     string getStatus();
     void setStatus(string status);
 };
 
 class Exchange_Student: public Student{
 public:
+    Exchange_Student(string id,string name, string email, int semester);
+    string getType();
     void displayProfile();
     bool grading();     //either pass or fail
 };
