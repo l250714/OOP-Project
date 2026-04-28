36482d5d0c52aed11b5c7f8d719b112ff924f466
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 14:23:06 +0500

    Update Management.h

--- Management.h
+++ Management.h
@@ -1,2 +1,15 @@
+#ifndef MANAGEMENT_H
+#define MANAGEMENT_H
+
 #include <iostream>
+#include "allheaders.h"
+#include "Venues.h"
 using namespace std;
+
+void Scheduler(){}
+void ConflictSolver(){}
+void TeacherFeedbacks(){}
+void SmartRegistration(){}
+
+
+#endif
