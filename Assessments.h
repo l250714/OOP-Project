45539d3a9f2715c8174d79f8def7edfda3365608
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 14:18:57 +0500

    Update Assessments.h

--- Assessments.h
+++ Assessments.h
@@ -5,30 +5,57 @@
 using namespace std; 
 
 class Assessments{
-    float weightage;
+    float weightage;        
     float max,min,avg;
     int num;    //assessment number
     float rawscore;
     int totalMarks;
 public:
+    Assessments();
 
+    virtual string getType()=0;
+    float getWeight();
+    float getMax();
+    float getMin();
+    float getAvg();
+    float getRawscore();
+    int getNum();
+    int getTMarks();
+
+    void setWeight(float weight);
+    void setMax(float max);
+    void setMin(float min);
+    void setAvg(float avg);
+    void setRawscore(float rawScore);
+    void setNum(int num);
+    void setTMarks(int total);
 };
 
 class Quiz: public Assessments{
-
+    
+public:
+   string getType();
+   
 };
 
 class Assignment: public Assessments{
-    
+
+public:
+   string getType();
 };
 
 class Exam: public Assessments{
     int num_questions;
     float* marks_perQ;
+public:
+    Exam();
+    string getType();
 };
 
 class Project: public Assessments{
-    
+
+public:
+    string getType();
 };
 
 #endif
