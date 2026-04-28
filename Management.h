e76d2fe261a0debbfc86abd60399717247d32f6d
l250714 <l250714@lhr.nu.edu.pk>
2026-04-28 21:23:27 +0500

    Update Management.h

--- Management.h
+++ Management.h
@@ -6,10 +6,31 @@
 #include "Venues.h"
 using namespace std;
 
-void Scheduler(){}
-void ConflictSolver(){}
-void TeacherFeedbacks(){}
-void SmartRegistration(){}
+
+class section_course{       //to connect each teacher with a course + section
+    Course course;
+    Teacher teacher;
+    Venue venue;
+    string sectionid, timings;
+public: 
+    section_course();
+    void setCourse(Course& obj);
+    void setTeacher(Teacher& obj);
+    void setVenue(Venue& obj);
+    void setsectionID(string id);
+    void setTimings(string timings);
+
+    Course& getCourse();
+    Teacher& getTeacher();
+    Venue& getVenue();
+    string getSectionID();
+    string getTimings();
+};
+
+void Scheduler();
+void ConflictSolver();
+void TeacherFeedbacks();
+void SmartRegistration();
 
 
 #endif
