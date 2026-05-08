a758ae748b6bf755a8390dbdfc9bee37eb3942a0
l250714 <l250714@lhr.nu.edu.pk>
2026-05-09 02:15:37 +0500

    Update main.cpp
    testing to see if all data is being read correctly (readAssessments needs help)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,85 @@
 #include <iostream>
-#include "allheaders.h"
+#include <vector>
+#include "DatabaseManager.h"
 using namespace std;
 
-/* cd "C:\Users\hp\OneDrive\Desktop\Uni study\Sem 2\OOP\Project" g++ -g Academic_Entities.cpp Assessments.cpp Courses.cpp DatabaseManager.cpp Management.cpp Venues.cpp main.cpp -o Project.exe
-./Project.exe */
+void StudentProfile() {
+    int choice;
+    while (true) {
+        cout << "\nPlease choose your action: 1. View Profile\n2. View Timetable\n3. View Courses\n4. View Marks\n5. Teacher Feedbacks\n6. View Transcript\n7. Registration\n8. Exam Schedule\n9. Close System\nYour choice: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            S_viewProfile();
+        case 2:
+            S_viewTimetable();
+        case 3:
+            S_viewCourses();
+        case 4:
+            S_viewMarks();
+        case 5:
+            S_feedback();
+        case 6: 
+            S_viewTranscript();
+        case 7:
+            S_registration();
+        case 8:
+            S_examSchedule();
+        case 9:
+            cout << "\nGoodbye! :)";
+            return;
+        }
+    }
+}
+
+void TeacherProfile() {
+    int choice;
+    while (true) {
+        cout << "\nPlease choose your action: 1. View Profile\n2. View Schedule\n3. View Classes\n4. Set Marks\n5. View Feedback Results\n6. View Class Average\n7. Close System\nYour choice: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            T_viewProfile();
+        case 2:
+            T_viewSchedule();
+        case 3:
+            T_viewClasses();
+        case 4:
+            T_setMarks();
+        case 5:
+            T_viewFeedback();
+        case 6:
+            T_viewAverage();
+        case 7:
+            cout << "\nGoodbye! :)";
+            return;
+        }
+    }
+}
 
 int main(){
-    readStudentdata();
-    cout<<"\nStudent data read";
+    /*readStudentdata();
+    cout<<endl<<"Student data read"; 
+    cout << endl << studentinfo[0]->getName();
     readCoursesdata();
-    cout<<"\nCourses data read";
+    cout<<endl<<"Courses data read"; 
+    cout << endl << courseinfo[0]->getName();
     readTeachersdata();
-    cout<<"\nTeachers data read";
+    cout<<endl<<"Teachers data read"; 
+    cout << endl << teacherinfo[0].getID();
     readVenuesdata();
-    cout<<"\nVenues data read";
+    cout<<endl<<"Venues data read"; 
+    cout << endl << venueinfo[0].getID();
     readSectionsdata();
-    cout<<"\nSections data read";
+    cout<<endl<<"Sections data read";
+    cout << endl << sectioninfo[0].getTimings();
     readWeightagesdata();
-    cout<<"\nWeightages data read";
+    cout<<endl<<"Weightages data read"; 
+    cout << endl << weightstore[0].exam;
     readAssessmentsdata();
-    cout<<"\nAssessments data read";
+    cout<<endl<<"Assessments data read";
+    cout << endl << assesssmentinfo[0]->getRawscore();*/
+
+
     return 0;
 }
