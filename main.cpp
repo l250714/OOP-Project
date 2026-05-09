ccc770dc85cec84b605b9288f7386e10b748053d
l250714 <l250714@lhr.nu.edu.pk>
2026-05-09 22:37:58 +0500

    Update main.cpp
    Finally began work on the actual functionalities

--- main.cpp
+++ main.cpp
@@ -3,17 +3,51 @@
 #include "DatabaseManager.h"
 using namespace std;
 
-void StudentProfile() {
+
+void S_viewProfile(Student* ptr) {                                                                  
+    cout << endl;
+    cout << "_____ _             _            _     _____            __ _ _" << endl;
+    cout << "/ ____| |           | |          | |   |  __ \\          / _(_) |" << endl;
+    cout << "| (___ | |_ _   _  __| | ___ _ __ | |_  | |__) | __ ___ | |_ _| | ___" << endl;
+    cout << "\\___ \\| __| | | |/ _` |/ _ \\ '_ \\| __| |  ___/ '__/ _ \\|  _| | |/ _ \\"<< endl;
+    cout << "____) | |_| |_| | (_| |  __/ | | | |_  | |   | | | (_) | | | | |  __/" << endl;
+    cout << "|_____/ \\__|\\__,_|\\__,_|\\___|_| |_|\\__| |_|   |_|  \\___/|_| |_|_|\\___|" << endl;
+    //Name, ID, Semester,Section, Email, gpa, number of courses
+    ptr->displayProfile();
+}
+void S_viewTimetable(Student* ptr) {
+    for (int i = 0;i < sectioninfo.size();i++) {
+        cout << sectioninfo[i].getCourseName()<<endl;
+    }
+    cout << "\nTimeTable";
+    for (int i = 0;i < sectioninfo.size();i++) {
+        if (sectioninfo[i].getSectionID() == ptr->getSection()) {
+            cout << endl<<sectioninfo[i].getCourseName() << "\t\t" << sectioninfo[i].getTimings() << "\t\tVenue: " << sectioninfo[i].getVenue().getID();
+        }
+    }
+    
+}
+void StudentProfile(Student* ptr) {
     int choice;
     while (true) {
+        cout << endl;
+        cout << "888b     d888          d8b               888b     d888" << endl;
+        cout << "8888b   d8888          Y8P               8888b   d8888" << endl;
+        cout << "88888b.d88888                            88888b.d88888" << endl;
+        cout << "888Y88888P888  8888b.  888 88888b.       888Y88888P888  .d88b.  88888b.  888  888" << endl;
+        cout << "888 Y888P 888     \"88b 888 888 \"88b      888 Y888P 888 d8P  Y8b 888 \"88b 888  888" << endl;
+        cout << "888  Y8P  888 .d888888 888 888  888      888  Y8P  888 88888888 888  888 888  888" << endl;
+        cout << "888   \"   888 888  888 888 888  888      888   \"   888 Y8b.     888  888 Y88b 888" << endl;
+        cout << "888       888 \"Y888888 888 888  888      888       888  \"Y8888  888  888  \"Y88888" << endl;
+                                                                                                    
         cout << "\nPlease choose your action: 1. View Profile\n2. View Timetable\n3. View Courses\n4. View Marks\n5. Teacher Feedbacks\n6. View Transcript\n7. Registration\n8. Exam Schedule\n9. Close System\nYour choice: ";
         cin >> choice;
         switch (choice) {
         case 1:
-            S_viewProfile();
-        case 2:
-            S_viewTimetable();
-        case 3:
+            S_viewProfile(ptr);
+       case 2:
+            S_viewTimetable(ptr);
+        /* case 3:
             S_viewCourses();
         case 4:
             S_viewMarks();
@@ -24,7 +58,7 @@ void StudentProfile() {
         case 7:
             S_registration();
         case 8:
-            S_examSchedule();
+            S_examSchedule();*/
         case 9:
             cout << "\nGoodbye! :)";
             return;
@@ -32,12 +66,12 @@ void StudentProfile() {
     }
 }
 
-void TeacherProfile() {
+void TeacherProfile(Teacher& obj) {
     int choice;
     while (true) {
         cout << "\nPlease choose your action: 1. View Profile\n2. View Schedule\n3. View Classes\n4. Set Marks\n5. View Feedback Results\n6. View Class Average\n7. Close System\nYour choice: ";
         cin >> choice;
-        switch (choice) {
+        /*switch (choice) {
         case 1:
             T_viewProfile();
         case 2:
@@ -53,33 +87,65 @@ void TeacherProfile() {
         case 7:
             cout << "\nGoodbye! :)";
             return;
-        }
+        }*/
     }
 }
 
 int main(){
-    /*readStudentdata();
-    cout<<endl<<"Student data read"; 
-    cout << endl << studentinfo[0]->getName();
-    readCoursesdata();
-    cout<<endl<<"Courses data read"; 
-    cout << endl << courseinfo[0]->getName();
-    readTeachersdata();
-    cout<<endl<<"Teachers data read"; 
-    cout << endl << teacherinfo[0].getID();
-    readVenuesdata();
-    cout<<endl<<"Venues data read"; 
-    cout << endl << venueinfo[0].getID();
+    readStudentdata();
     readSectionsdata();
-    cout<<endl<<"Sections data read";
-    cout << endl << sectioninfo[0].getTimings();
+    readCoursesdata();
+    readTeachersdata();
+    readVenuesdata();
     readWeightagesdata();
-    cout<<endl<<"Weightages data read"; 
-    cout << endl << weightstore[0].exam;
     readAssessmentsdata();
-    cout<<endl<<"Assessments data read";
-    cout << endl << assesssmentinfo[0]->getRawscore();*/
+    Linking();
 
+    string ID;
+    int index;
+    while (true) {
+        cout << endl;
+        cout << "_    ____ ____ _ _  _" << endl;
+        cout << "|    |  | | __ | |\\ |" << endl;
+        cout << "|___ |__| |__] | | \\|" << endl;
+        cout << "\nUsername: ";
+        cin >> ID;      //input validation
+        bool found = false;
+        if (ID[0] == 'S') {
+            for (int i = 0;i < studentinfo.size();i++) {
+                if (studentinfo[i]->getID() == ID) {
+                    found = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (found) {
+                StudentProfile(studentinfo[index]);
+                break;
+            }
+            else {
+                cout << "\nError: ID does not exist. Please Try Again.";
+            }
+        }
+        else {
+            for (int i = 0;i < teacherinfo.size();i++) {
+                if (teacherinfo[i].getID() == ID) {
+                    found = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (found) {
+                TeacherProfile(teacherinfo[index]);
+                break;
+            }
+            else {
+                cout << "\nError: ID does not exist. Please Try Again.";
+            }
+        }
+    }
+   
+    
 
     return 0;
 }
