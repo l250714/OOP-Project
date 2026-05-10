083f5267193185887c46debecea12c59b7514045
l250714 <l250714@lhr.nu.edu.pk>
2026-05-10 21:06:48 +0500

    Update Courses.h

--- Courses.h
+++ Courses.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef COURSES_H
 #define COURSES_H
 
@@ -8,92 +9,115 @@
 using namespace std;
 
 class Student;
-
 //think about schedule and timings and venue
-class Course{   //base class
+class Course {   //base class
 protected:      //makes data members accessible in child classes
     string ID;
     string name;
     int TQuizzes, TExams, TAssignments;     //total of each assessment type (in class)
     float exam_duration;
-    int credithr;      
+    int credithr;
     float batch_average;    //out of 100 (in class)
     string teacherID;
     vector <Student*> students;
 public:
     Course();
-    void set_ID(const char* id);
-    void setname(const char* name);
+    void set_ID(string id);
+    void set_teacherID(string id);
+    void setname(string name);
     void setExamduration(float time);
     void setStudent(Student& obj);
     string getID();
     string getName();
     float getExamDuration();
+    string get_teacherid();
     Student& getStudent(int i);
+    virtual int getTQuizzes()=0;
+    virtual int getTExams()=0;
+    virtual int getTAssignments()=0;
+    virtual int getCredits()=0;
+    virtual string getType() = 0;
+    //vector<Student*> getallStudents();
     virtual ~Course();
-    
+
 };
 
 //----Types of Courses-----
 
-class Core: public Course{
+class Core : public Course {
     vector<Quiz> quizzes;
     vector<Exam> exams;
     vector <Assignment> assignments;
     float points;
-    char*grade;
+    string grade;
 
 public:
     Core();
     void setpoints(float points);
-    void setgrade(char* grade);
+    void setgrade(string grade);
     void setQuiz(Quiz& obj);
     void setExam(Exam& obj);
     void setAssignment(Assignment& obj);
+
     float getpoints();
     string getgrade();
     Exam& getExam(int i);
     Quiz& getQuiz(int i);
+    string getType();
     Assignment& getAssignment(int i);
+    int getCredits();
+    int getTQuizzes();
+    int getTExams();
+    int getTAssignments();
     ~Core();
 
 };
 
-class Elective: public Course{
+class Elective : public Course {
     vector <Assignment> assignments;
-    Project final_project;
     vector<Quiz> quizzes;
     float points;
-    char*grade;
+    string grade;
 public:
     Elective();
     void setpoints(float points);
     void setQuiz(Quiz& obj);
-    void setgrade(char* grade);
-    void setLabtasks(Assignment& obj);
+    void setgrade(string grade);
+    void setAssignment(Assignment& obj);
     //void setProject();
+
     float getpoints();
     string getgrade();
-     Quiz& getQuiz(int i);
-    Assignment& getLabTasks(int i);
+    Quiz& getQuiz(int i);
+    string getType();
+    Assignment& getAssignment(int i);
+    int getCredits();
+    int getTQuizzes();
+    int getTExams();
+    int getTAssignments();
     ~Elective();
 };
 
-class Lab: public Course{
+class Lab : public Course {
     vector<Quiz> quizzes;
     vector <Assignment> Lab_tasks;
     float points;
-    char*grade;
+    string grade;
 public:
     Lab();
     void setpoints(float points);
-    void setgrade(char* grade);
+    void setgrade(string grade);
     void setQuiz(Quiz& obj);
     void setAssignment(Assignment& obj);
     Quiz& getQuiz(int i);
-    Assignment& getAssignment(int i);
     float getpoints();
     string getgrade();
+    string getType();
+    int getCredits();
+    int getTQuizzes();
+    int getTExams();
+    Assignment& getAssignment(int i);
+    int getTAssignments();
     ~Lab();
 };
 
