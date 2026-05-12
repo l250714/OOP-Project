564f4b3fb24b4ebc2dc62f575a7b8c0fcd259f46
l250714 <l250714@lhr.nu.edu.pk>
2026-05-12 21:55:54 +0500

    Update Venues.h

--- Venues.h
+++ Venues.h
@@ -1,16 +1,17 @@
+#pragma once
 #ifndef VENUES_H
 #define VENUES_H
 
 #include <iostream>
 using namespace std;
 
-class Venue{
+class Venue {
     string ID;
     int capacity;
     bool has_computers;
 public:
     Venue();
-    void setID(string);
+    void setID(string ID);
     void setCapacity(int cap);
     void setComputer(bool comp);
     string getID();
