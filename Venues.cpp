0574ca9a7941afb510c3c34932fcf83740aa4ab8
l250714 <l250714@lhr.nu.edu.pk>
2026-05-12 21:58:10 +0500

    Update Venues.cpp

--- Venues.cpp
+++ Venues.cpp
@@ -3,34 +3,29 @@
 #include "Venues.h"
 using namespace std;
 
-Venue::Venue(){
-    ID="none";
-    capacity=0;
-    has_computers=0;
+Venue::Venue() {
+    ID = "none";
+    capacity = 0;
+    has_computers = 0;
 }
-void Venue::setID(string ID){
+void Venue::setID(string ID) {
     this->ID = ID;
 }
-void Venue::setCapacity(int cap){
-    if(cap>0 && cap<300){
-        capacity=cap;
-    }
-    else{
-        cout<<"\nError: Venue capacity is invalid.";
-    }
+void Venue::setCapacity(int cap) {
+    capacity = cap;
 }
-void Venue::setComputer(bool comp){
-    has_computers=comp;
+void Venue::setComputer(bool comp) {
+    has_computers = comp;
 }
 
-string Venue::getID(){
+string Venue::getID() {
     return ID;
 }
-int Venue::getCapacity(){
+int Venue::getCapacity() {
     return capacity;
 }
-bool Venue::getComputers(){
+bool Venue::getComputers() {
     return has_computers;
 }
-Venue::~Venue(){}
+Venue::~Venue() {}
 
